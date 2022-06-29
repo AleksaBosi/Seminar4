ed6cdc77cc5f389aaaba2ff75a80bad5f446ece1
Aleksa Bosi <aleksabosi@gmail.com>
2022-06-29 12:58:42 +0300

    Seminar 4

---
+++ Task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите первое число.");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine($"Первое число в натуральной степени второго числа {result}");
---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// void Main()
+// {
+//     Console.WriteLine("Введите число.");
+//     int number = Convert.ToInt32(Console.ReadLine());
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result = result + number % 10;
+//         number /= 10;
+//     }
+//     Console.WriteLine($"Сумма цифр в числе {result}");
+// }
+// Main();
+
+void Main()
+{
+    Console.WriteLine("Введите число.");
+    string number = Console.ReadLine();
+    int res = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        res += Int32.Parse(number[i].ToString());
+    }
+    Console.WriteLine($"Сумма цифр в числе {res}");
+}
+Main();
